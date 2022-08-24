2787f8aa9cbf3fab6e9b91b6e5756f7e12cfcec7
kamal <kamal.latwal@binmile.com>
2022-08-24 15:35:01 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Logistic from "./components/Logistic/Logistic";
+import CardSection from "./components/CardSection/CardSection";
+import TrackingSection from "./components/TrackingSection/TrackingSection";
+import CompanyProject from "./components/CompanyProject/CompanyProject";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Hero />
+      <Logistic />
+      <CardSection />
+      <TrackingSection />
+      <CompanyProject />
+      <Footer />
     </div>
   );
 }
